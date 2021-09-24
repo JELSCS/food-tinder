206a54161274fd6ee8ff3235d9882a94080032b0
lkpham <levanpham10@gmail.com>
2021-09-24 15:41:40 -0700

    Add A4
    Ideally, scrape Yelp for top restaurants in area

--- Levan/src/main/java/org/example/App.java
+++ Levan/src/main/java/org/example/App.java
@@ -1,13 +1,20 @@
 package org.example;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+        System.out.println(doc.title());
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        for (Element headline : newsHeadlines) {
+            System.out.println( headline.attr("title")+", "+ headline.absUrl("href"));
+        }
+
     }
 }
