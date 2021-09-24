40e642ea9f8f36d9f3224279b9c7b88a6672e52c
Jonathan Galon <jonjongalon@gmail.com>
2021-09-23 20:09:25 -0700

    added A4

--- Jonathan/my-app/src/main/java/com/mycompany/app/App.java
+++ Jonathan/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,9 @@
 package com.mycompany.app;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
 
 /**
  * Hello world!
@@ -6,8 +11,19 @@ package com.mycompany.app;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args )throws IOException
     {
         System.out.println( "Hello World!" );
+  	Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+        log(doc.title());
+
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        for (Element headline : newsHeadlines) {
+            log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
+        }
+    }
+
+    private static void log(String msg, String... vals) {
+        System.out.println(String.format(msg, vals));
     }
 }
