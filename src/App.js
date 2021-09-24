b87f1b01f2f70f76c986f97833f6300e6dda1ba9
Jonathan Galon <jonjongalon@gmail.com>
2021-09-23 20:04:52 -0700

    added library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import HelloPage from './pages/Hello';
+import ByePage from './pages/Bye';
 
 function App() {
   return (
     <div className="App">
-      <h>"Team Members"</h>
+      <Switch>
+        <Route path='/hello'>
+          <HelloPage />
+        </Route>
+        <Route path='/bye'>
+          <ByePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
