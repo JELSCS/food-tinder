7f19e134616aba52f2b16ef44ca8dc5b193e41a9
Erin Baquiran <r.baquiran11@gmail.com>
2021-09-23 17:03:50 -0700

    Russ Erin Baquiran added jsoup with maven

---
+++ src/main/java/TestMaven.java
@@ -0,0 +1,14 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import javax.swing.*;
+
+public class TestMaven{
+    public static void main(){
+        //Jsoup;
+
+
+
+    }
+}                                                   
