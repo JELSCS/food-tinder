42c32ad48d38be7a28f0ad04f0bf3681dafa8b07
sisianteymoorian <89214324+sisianteymoorian@users.noreply.github.com>
2021-10-18 11:15:55 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'antd/dist/antd.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// Find information about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
